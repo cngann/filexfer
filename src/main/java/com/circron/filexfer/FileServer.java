c341d00cb3f3299a4f4453c6ab0a478d6c71f6a5
chris gann <cngann@gmail.com>
2021-07-14 19:21:18 -0700

    Removed example

---
+++ src/main/java/com/circron/filexfer/FileServer.java
@@ -0,0 +1,70 @@
+package com.circron.filexfer;
+
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@SuppressWarnings("unused") public class FileServer implements Runnable {
+    protected int serverPort;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
+    protected FileTransferConfig fileTransferConfig = FileTransferConfig.getInstance();
+    Logger logger = Utils.getLogger(this.getClass());
+
+    public FileServer() {
+        this.serverPort = fileTransferConfig.getPort();
+    }
+
+    public void run() {
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+        openServerSocket();
+        while (!isStopped()) {
+            Socket clientSocket;
+            try {
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if (isStopped()) {
+                    logger.debug("Server Stopped.");
+                    return;
+                }
+                String message = "Error accepting client connection";
+                logger.error(message);
+                throw new RuntimeException(message, e);
+            }
+            new Thread(new ReceiveFile(clientSocket)).start();
+        }
+        logger.info("Server Stopped.");
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop() {
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
+            logger.info("Stopping the server");
+        } catch (IOException e) {
+            String message = "Error closing server";
+            logger.error(message);
+            throw new RuntimeException(message, e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+            logger.info("Server started on port " + this.serverPort);
+        } catch (IOException e) {
+            String message = "Cannot open port " + this.serverPort;
+            logger.error(message);
+            throw new RuntimeException(message, e);
+        }
+    }
+}
---
+++ src/main/java/com/circron/filexfer/ReceiveFile.java
@@ -0,0 +1,77 @@
+package com.circron.filexfer;
+
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ReceiveFile implements Runnable {
+    Socket socket;
+    FileTransferConfig fileTransferConfig = FileTransferConfig.getInstance();
+    Logger logger = Utils.getLogger(this.getClass());
+
+    public ReceiveFile(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            int number = dataInputStream.readInt();
+            logger.debug("Number of Files to be received: " + number);
+            for (int i = 0; i < number; i++) {
+                String filename = Utils.getFilePath(fileTransferConfig.getDestinationPath(), dataInputStream.readUTF());
+                boolean isDirectory = dataInputStream.readBoolean();
+                boolean isEncrypted = dataInputStream.readBoolean();
+                long fileSize = dataInputStream.readLong();
+                logger.debug("Receiving " + (isDirectory ? "directory" : "file") + ": " + filename);
+                logger.debug("Receiving Size: " + fileSize);
+                logger.debug("Is directory: " + isDirectory);
+                logger.debug("Is encrypted: " + isEncrypted);
+                if (!isDirectory) {
+                    writeFile(filename, fileSize, dataInputStream);
+                    if (fileTransferConfig.isEncrypted()) {
+                        handleEncryptedFile(filename);
+                    }
+                } else {
+                    boolean dirExists = new File(filename).mkdirs();
+                    if (dirExists) {
+                        logger.info("Directory already exists");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            logger.error("IOException");
+            e.printStackTrace();
+        }
+    }
+
+    public void handleEncryptedFile(String filename) {
+        try {
+            File file = FileDecrypt.decryptFile(new File(filename));
+            logger.debug("File " + file.getPath() + " has been decrypted");
+            boolean deleted = new File(filename).delete();
+            if (deleted) {
+                logger.debug("Original encrypted file (" + filename + ") has been deleted");
+            }
+        } catch (Exception e) {
+            logger.error("Could not decrypt file\n" + e.getMessage());
+        }
+    }
+
+    public void writeFile(String filename, long fileSize, DataInputStream dataInputStream) throws IOException {
+        int length;
+        byte[] buf = new byte[fileTransferConfig.getStreamBufferLength()];
+        FileOutputStream fos = new FileOutputStream(filename);
+        logger.error("File not found: " + filename);
+        while (fileSize > 0 && (length = dataInputStream.read(buf, 0, (int)Math.min(buf.length, fileSize))) != -1) {
+            fos.write(buf, 0, length);
+            fileSize -= length;
+        }
+        fos.close();
+    }
+}
